93736d37490f447f0fa7bdfd4bf44df8d3ade6f5
Jonas Lergell <jonaslergell@gmail.com>
2025-09-08 15:00:57 +0200

    fix conf

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://yourdomain.com/", // replace this with your deployed domain
+  website: "https://thrawny.github.io", // replace this with your deployed domain
   author: "Jonas Lergell",
-  profile: "https://yourdomain.com/",
-  desc: "Personal website and blog",
-  title: "thrawny.github.io",
+  profile: "https://thrawny.github.io",
+  desc: "Personal website and blog about software development",
+  title: "Jonas Lergell",
   ogImage: "og-image.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -12,12 +12,12 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: false, // Enable when you set up your GitHub repo
+    enabled: true, // Enable when you set up your GitHub repo
     text: "Edit page",
-    url: "https://github.com/yourusername/yourrepo/edit/main/",
+    url: "https://github.com/thrawny/personal-website/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "America/New_York", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Europe/Stockholm", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
